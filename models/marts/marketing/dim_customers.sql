526f2b697b4532e77ea642900e9019e909b4e3c2
Joel Gracek <superjoelpg@gmail.com>
2026-09-22 18:44:33 +0000

    fix: rename orders column

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -16,7 +16,7 @@ orders as (
 customer_orders as (
 
     select
-        order_user_id as customer_id,
+        customer_id,
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
--- models/staging/jaffle-shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle-shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,6 @@
 select
     id as order_id,
-    user_id as order_user_id,
+    user_id as customer_id,
     order_date,
     status as order_status
 
